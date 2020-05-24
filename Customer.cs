d7d77feb4d4843880b8d69046559508810070c9c
phanupong2541 <phanupong2541@hotmail.com>
2020-05-24 19:14:02 +0700

    HomeWorkArrayList

---
+++ Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloConstructor
+{
+    class Customer
+    {
+        string firstName = "";
+        string lastName = "";
+
+        public Customer()
+        {
+            Console.WriteLine("Hello !!!!");
+        }
+        public Customer(string firstName,string lastName)
+        {
+            Console.WriteLine($"Hello {firstName} {lastName}"); ;
+        }
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+    }
+}
